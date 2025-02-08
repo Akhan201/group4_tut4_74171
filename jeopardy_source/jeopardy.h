18f4e58f4b16259ae2db5edf5d5878dd2bed3ecf
SQPMLabs-thaq <tahnia.haq@ontariotechu.net>
2025-02-08 11:36:49 -0500

    Update jeopardy.h
    - add groups member names + student ids

--- jeopardy_source/jeopardy.h
+++ jeopardy_source/jeopardy.h
@@ -1,7 +1,9 @@
 /*
  * Tutorial 4 Jeopardy Project for SOFE 3950U / CSCI 3020U: Operating Systems
  *
- * Copyright (C) 2015, <GROUP MEMBERS>
+ * Copyright (C) 2015, <Nuha Tahnia Haq (100867378), Laiba Asif (100872058), 
+ * Arzika Khan (100753164), Ubong Offiong (100866634)>
+ *
  * All rights reserved.
  *
  */
