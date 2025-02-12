cc3a5ff3040d2261f5c47d5d2f311c8fcf8135d1
Laiba Asif <160992863+Lxiba@users.noreply.github.com>
2025-02-12 04:11:32 +0000

    jeopardy.h

--- jeopardy_source/jeopardy.h
+++ jeopardy_source/jeopardy.h
@@ -7,15 +7,12 @@
  * All rights reserved.
  *
  */
-#ifndef JEOPARDY_H_
-#define JEOPARDY_H_
 
-#define MAX_LEN 256
+#ifndef JEOPARDY_H
+#define JEOPARDY_H
 
-// Processes the answer from the user containing what is or who is and tokenizes it to retrieve the answer.
-extern void tokenize(char *input, char **tokens);
+#include "questions.h"
+#include "players.h"
 
-// Displays the game results for each player, their name and final score, ranked from first to last place
-extern void show_results(player *players, int num_players);
-
-#endif /* JEOPARDY_H_ */
+void play_game(Player players[PLAYER_COUNT], Question questions[CATEGORY_COUNT]>
+#endif
