47c6c3be73022d48cca226bb8df031431afd1a85
Ubong-malware <102366603+Ubong-malware@users.noreply.github.com>
2025-02-11 19:42:57 -0500

    Update players.c

--- jeopardy_source/players.c
+++ jeopardy_source/players.c
@@ -15,6 +15,10 @@
 // Returns true if the player name matches one of the existing players
 bool player_exists(player *players, int num_players, char *name)
 {
+    for (int i = 0; i < num_players; i++) {
+        if (strcmp(players[i].name, name) == 0) {
+            return true;
+        }
     return false;
 }
 
@@ -22,5 +26,10 @@ bool player_exists(player *players, int num_players, char *name)
 // player given their name
 void update_score(player *players, int num_players, char *name, int score)
 {
-    
+     for (int i = 0; i < num_players; i++) {
+        if (strcmp(players[i].name, name) == 0) {
+            players[i].score += score; // Add points to the player's score
+            return;
+        }
+     }
 }
