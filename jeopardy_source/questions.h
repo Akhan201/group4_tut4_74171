5659eaa5f1df088c77890e8acefb413c87a64028
Ubong-malware <102366603+Ubong-malware@users.noreply.github.com>
2025-02-12 00:49:57 -0500

    Update questions.h

--- jeopardy_source/questions.h
+++ jeopardy_source/questions.h
@@ -11,22 +11,14 @@
 #ifndef QUESTIONS_H
 #define QUESTIONS_H
 
-#include <stdbool.h>
+void displayCategoriesAndQuestions();
+int isQuestionAnswered(const char* category, int dollarAmount);
+void getQuestion(const char* category, int dollarAmount);
+int checkAnswer(const char* answer, const char* category, int dollarAmount);
+void displayCorrectAnswer(const char* category, int dollarAmount);
+void markQuestionAsAnswered(const char* category, int dollarAmount);
+int allQuestionsAnswered();
 
-#define CATEGORY_COUNT 3
-#define QUESTIONS_PER_CATEGORY 3
-
-// Structure to store a Jeopardy question
-typedef struct {
-    char category[500];
-    int value;
-    char question[2560];
-    char answer[500];
-    bool answered;
-} Question;
-
-void initialize_questions(Question questions[CATEGORY_COUNT][QUESTIONS_PER_CATEGORY]);
-void display_questions(const Question questions[CATEGORY_COUNT][QUESTIONS_PER_CATEGORY]);
-bool validate_question(const Question questions[CATEGORY_COUNT][QUESTIONS_PER_CATEGORY], const char *category, int value);
+#endifar *category, int value);
 bool check_answer(Question *q, const char *player_answer);
 #endif
